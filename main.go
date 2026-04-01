8a663ccd617f137e974920c8bc6ec9c88a2bed8f
kyle <kyle@lindsay.gr>
2026-04-01 17:40:11 +0100

    added commit command

--- main.go
+++ main.go
@@ -20,13 +20,31 @@ func main() {
 		if argsLength == 2 {
 			if err := initialise(); err != nil {
 				fmt.Println(err)
+				return
 			}
+			fmt.Println("Repository initialised")
 		} else {
 			fmt.Println("init requires no parameters")
 			return
 		}
+
+	case "commit":
+		if argsLength == 3 {
+			message := os.Args[2]
+
+			id, err := createCommit(".", message)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println("Created commit", id)
+		} else {
+			fmt.Println("commit requires a message")
+			return
+		}
 	default:
-		fmt.Println("Unknown command " + command)
+		fmt.Println("Unknown command", command)
 		return
 	}
 }
