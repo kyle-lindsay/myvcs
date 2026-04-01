0790a21c4253dd44348be6f6b2651434599abc06
kyle <kyle@lindsay.gr>
2026-04-01 14:59:37 +0100

    added build snapshot function

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -63,6 +65,47 @@ func initialise() error {
 
 func buildSnapshot(root string) ([]FileEntry, error) {
 	entries := []FileEntry{}
+
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() && d.Name() == ".myvcs" {
+			return filepath.SkipDir
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(root, path)
+
+		if err != nil {
+			return err
+		}
+
+		fileHash, err := hashFile(path)
+
+		if err != nil {
+			return err
+		}
+
+		entry := FileEntry{
+			Path: relPath,
+			Hash: fileHash,
+		}
+
+		entries = append(entries, entry)
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return entries, nil
 }
 
 func hashFile(path string) (string, error) {
